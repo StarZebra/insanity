7ff92efb39b22fd5387dc6a232b4ae1394dffc0e
StarZebra <star@zebra.dev>
2023-05-07 14:38:54 +0200

    Initial Commit

---
+++ src/main/java/me/starzebra/Insanity.java
@@ -0,0 +1,15 @@
+package me.starzebra;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "insanity", version = "0.1")
+
+public class Insanity {
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
+    }
+}
