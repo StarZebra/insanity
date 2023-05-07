7713253c375e5107c9e23dfdbb690cd8805e80af
StarZebra <star@zebra.dev>
2023-05-07 17:29:11 +0200

    Some stuff

--- src/main/java/me/starzebra/features/ItemUpdateFix.java
+++ src/main/java/me/starzebra/features/ItemUpdateFix.java
@@ -6,12 +6,9 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class ItemUpdateFix {
 
     public static boolean toggled = false;
-    public static boolean saveOnWorldSwap = false;
 
     @SubscribeEvent
     public void onWorldLoad(WorldEvent.Load event){
-        if(!saveOnWorldSwap){
-            toggled = false;
-        }
+        toggled = false;
     }
 }
